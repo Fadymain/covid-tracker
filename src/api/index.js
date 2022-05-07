f5dc5340278d9ff1c92d06689960e01e4458a100
Adetola <adetolafadairo@gmail.com>
2022-05-06 21:25:49 -0700

    creating and exporting fetchDailyData function

--- src/api/index.js
+++ src/api/index.js
@@ -12,4 +12,13 @@ export const fetchData = async () => {
   } catch (error) {
 
   }
-}
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    console.log(data);
+  } catch (error){
+
+  }
+} 
